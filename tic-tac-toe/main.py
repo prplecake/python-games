0351c6a75452a9fac5e4f99feb9f19e26eb06848
Matthew Jorgensen <matthew@jrgnsn.net>
2022-03-23 12:31:30 -0500

    Write logical functions

--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -103,8 +103,95 @@ class TicTacToe:
         score_text = 'Click to play again\n'
         self.canvas.create_text(size_of_board / 2, 15 * size_of_board / 16, font='cmr 20 bold', fill='gray', text=score_text)
 
+    ###################
+    # Logical Functions
+    ###################
+
+    def convert_logical_to_grid_position(self, logical_position):
+        logical_position = np.array(logical_position, dtype=int)
+        return (size_of_board / 3) * logical_position + size_of_board / 6
+
+    def convert_grid_to_logical_position(self, grid_position):
+        grid_position = np.array(grid_position)
+        return np.array(grid_position // (size_of_board / 3), dtype=int)
+
+    def is_grid_occupied(self, logical_position):
+        if self.board_status[logical_position[0]][logical_position[1]] == 0:
+            return False
+        else:
+            return True
+
+    def is_winner(self, player):
+        player = -1 if player == 'X' else 1
+
+        # Three in a row
+        for i in range(3):
+            if self.board_status[i][0] == self.board_status[i][1] == self.board_status[i][2] == player:
+                return True
+            if self.board_status[0][i] == self.board_status[1][i] == self.board_status[2][i] == player:
+                return True
+        
+        # Diagonals
+        if self.board_status[0][0] == self.board_status[1][1] == self.board_status[2][2] == player:
+            return True
+        
+        if self.board_status[0][2] == self.board_status[1][1] == self.board_status[2][0] == player:
+            return True
+        
+        return False
+
+    def is_tie(self):
+        r, c = np.where(self.board_status == 0)
+        tie = False
+        if len(r) == 0:
+            tie = True
+        return tie
+    
+    def is_gameover(self):
+        # Either someone won or all grid spaces are occupied
+        self.X_wins = self.is_winner('X')
+        if not self.X_wins:
+            self.O_wins = self.is_winner('O')
+        
+        if not self.O_wins:
+            self.tie = self.is_tie()
+        
+        gameover = self.X_wins or self.O_wins or self.tie
+
+        if self.X_wins:
+            print('X wins')
+        if self.O_wins:
+            print('O wins')
+        if self.tie:
+            print('It\'s a tie')
+
+        return gameover
+
     def click(self, event):
-        grid_position = 
+        grid_position = [event.x, event.y]
+        logical_position = self.convert_grid_to_logical_position(grid_position)
+
+        if not self.reset_board:
+            if self.player_X_turns:
+                if not self.is_grid_occupied(logical_position):
+                    self.draw_X(logical_position)
+                    self.board_status[logical_position[0]][logical_position[1]] = -1
+                    self.player_X_turns = not self.player_X_turns
+            else:
+                if not self.is_grid_occupied(logical_position):
+                    self.draw_O(logical_position)
+                    self.board_status[logical_position[0]][logical_position[1]] = 1
+                    self.player_X_turns = not self.player_X_turns
+
+            # check if game is concluded
+            if self.is_gameover():
+                self.display_gameover()
+
+        else:
+            self.canvas.delete('all')
+            self.play_again()
+            self.reset_board = False
+
 
 if __name__ == '__main__':
     game_instance = TicTacToe()
